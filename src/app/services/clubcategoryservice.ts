beaede4e78ee731816a8c701c96fa81d097b2e97
erengulum <b21627265@cs.hacettepe.edu.tr>
2021-05-09 14:02:51 +0300

    club-request-added

--- src/app/services/clubcategoryservice.ts
+++ src/app/services/clubcategoryservice.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from "../../environments/environment";
 import { ClubCategoryDto } from '../entity/clubcategory';
+import { ClubCategoryRequestDto } from '../entity/ClubCategoryRequestDto';
 
 
 @Injectable({ providedIn: 'root' })
@@ -14,16 +15,37 @@ export class ClubCategoryService {
     }
 
     deleteById(categoryId:string){
-        console.log("servise geldi:",categoryId);
         return this.http.delete<any>(`${environment.API_BASE_PATH}/clubcategories/delete/${categoryId}`);
 
     }
 
     saveClubCategory(clubCategory:ClubCategoryDto){
-        console.log("servise geldi:",clubCategory);
         return this.http.post<any>(`${environment.API_BASE_PATH}/clubcategories/create`,clubCategory);
 
     }
 
+
+    saveClubCategoryRequest(clubCategoryRequestDto:ClubCategoryRequestDto){
+        return this.http.post<any>(`${environment.API_BASE_PATH}/clubcategories/request/create`,clubCategoryRequestDto);
+
+    }
+    getAllRequests() {
+        return this.http.get<ClubCategoryRequestDto[]>(`${environment.API_BASE_PATH}/clubcategories/request/all`);
+    }
+
+    deleteRequestById(requestId:string){
+        console.log("req silme islemi basladi");
+        return this.http.delete<any>(`${environment.API_BASE_PATH}/clubcategories/request/delete/${requestId}`);
+
+    }
+
+    convertRequestToClubCategory(clubCategoryRequestDto:ClubCategoryRequestDto){
+        return this.http.post<any>(`${environment.API_BASE_PATH}/clubcategories/request/create/club`,clubCategoryRequestDto);
+    }
+
+
+    voteForClubRequest(requestId:string){
+        return this.http.get<any>(`${environment.API_BASE_PATH}/clubcategories/request/vote/${requestId}`);
+    }
 
 }
